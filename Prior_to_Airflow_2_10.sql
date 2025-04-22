1c1fb02092fcaaf6b4359410e4f10d3975ec5e33
Cody Rich <83995939+TheCodyRich@users.noreply.github.com>
2025-04-22 11:58:35 -0400

    Create Prior_to_Airflow_2_10.sql

---
+++ Prior_to_Airflow_2_10.sql
@@ -0,0 +1,10 @@
+-- The below queries will produce the sum duration of task instances in minutes for a given month per Airflow environment. Adjust the months to get compute minutes for the most recent 3 months. Read access to the Airflow metadata DB is required.
+SELECT 
+    SUM(EXTRACT(EPOCH FROM (end_date - start_date)) / 60) AS total_duration_minutes
+FROM 
+    task_instance
+WHERE 
+    start_date >= DATE '2024-03-01'
+    AND end_date < DATE '2024-04-01'
+    AND end_date IS NOT NULL
+    AND start_date IS NOT NULL;
